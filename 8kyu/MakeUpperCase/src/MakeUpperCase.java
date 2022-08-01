542d525c6e6363d470af548c156facd235a156d8
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-01 23:52:03 +0300

    project MakeUpperCaseAdded

---
+++ 8kyu/MakeUpperCase/src/MakeUpperCase.java
@@ -0,0 +1,9 @@
+public class MakeUpperCase {
+    public static void main(String[] args) {
+        String a ="Test";
+        System.out.println(MakeUpperCase(a));
+    }
+    private static String MakeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
