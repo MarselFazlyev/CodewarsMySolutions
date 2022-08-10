2ce6fa94a4961136fb0ba5c7b5322c449f87acb4
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-11 01:33:35 +0300

    new task added

---
+++ 7kyu/SumOfOddNumbers/src/RawSumOddNumbers.java
@@ -0,0 +1,21 @@
+import java.util.stream.IntStream;
+
+public class RawSumOddNumbers {
+    public static void main(String[] args) {
+        int testNumber = 10;
+        System.out.println(rowSumOddNumbers(testNumber));
+    }
+
+    private static int rowSumOddNumbers(int n) {
+        if (n <= 0) throw new RuntimeException("Номер строки должн быть положительным целым числом!");
+        int numbersBelow = (n - 1) * n / 2;
+         /* арифметическая прогрессия подсчитывает общее количество предыдущих
+         элементов строки n по формуле a = a(a+1)/2 ( сколько надо их пропустить в стриме */
+        return IntStream.iterate(1, x -> x + 2)
+                .skip(numbersBelow)
+                .limit(n).sum();
+    }
+
+}
+
+
